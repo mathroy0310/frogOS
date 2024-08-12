9d4177043e3ca8783983e65d8f4613d839b3664a
mathroy0310 <mathroy0310@gmail.com>
2024-08-12 02:44:48 -0400

    fix skill issue

---
+++ kernel/kernel/Panic.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                             _              */
+/*                                                 __   ___.--'_\`.           */
+/*   Panic.cpp                                    ( _\`.' -   'o\` )          */
+/*                                                _\\.'_'      _.-'           */
+/*   By: mathroy0310 <maroy0310@gmail.com>       ( \`. )    //\\\`            */
+/*                                                \\_'-`---'\\__,             */
+/*   Created: 2024/08/12 02:17:13 by mathroy0310   \`        `-\\             */
+/*   Updated: 2024/08/12 02:38:46 by mathroy0310    `                         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <kernel/Panic.h>
+
+namespace Kernel {
+
+void dump_stacktrace() {
+	struct stackframe {
+		stackframe *ebp;
+		uint64_t    eip;
+	};
+
+	stackframe *frame = (stackframe *) __builtin_frame_address(0);
+	FROG::Formatter::print(Serial::serial_putc, "\e[36mStack trace:\r\n");
+	while (frame) {
+		FROG::Formatter::print(Serial::serial_putc, "    {}\r\n", (void *) frame->eip);
+		frame = frame->ebp;
+	}
+}
+
+} // namespace Kernel
