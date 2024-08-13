8a387f6c221341156ec0dee613b7b094ae39bca5
mathroy0310 <mathroy0310@gmail.com>
2024-08-12 20:43:58 -0400

    debug stacktrace detects if panic

--- kernel/kernel/Debug.cpp
+++ kernel/kernel/Debug.cpp
@@ -6,7 +6,7 @@
 /*   By: mathroy0310 <maroy0310@gmail.com>       ( \`. )    //\\\`            */
 /*                                                \\_'-`---'\\__,             */
 /*   Created: 2024/08/12 18:20:21 by mathroy0310   \`        `-\\             */
-/*   Updated: 2024/08/12 18:40:02 by mathroy0310    `                         */
+/*   Updated: 2024/08/12 20:43:20 by mathroy0310    `                         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,25 @@
 namespace Debug {
 void DumpStackTrace() {
 	struct stackframe {
-		stackframe *ebp;
-		uintptr_t   eip;
+		stackframe *rbp;
+		uintptr_t   rip;
 	};
 
 	stackframe *frame = (stackframe *) __builtin_frame_address(0);
+	if (!frame) {
+		dprintln("Could not get frame address");
+		return;
+	}
+	uintptr_t first_rip = frame->rip;
 	FROG::Formatter::print(Debug::putchar, "\e[36mStack trace:\r\n");
 	while (frame) {
-		FROG::Formatter::print(Debug::putchar, "    {}\r\n", (void *) frame->eip);
-		frame = frame->ebp;
+		FROG::Formatter::print(Debug::putchar, "    {}\r\n", (void *) frame->rip);
+		frame = frame->rbp;
+
+		if (frame && frame->rip == first_rip) {
+			derrorln("looping kernel panic :(");
+			return;
+		}
 	}
 }
 void putchar(char ch) {
