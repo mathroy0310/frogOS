801297cd1ab51383cb13d2d5f2ed42fa723a0c3f
mathroy0310 <mathroy0310@gmail.com>
2024-08-22 11:56:09 -0400

    add check in format.sh
    looks if youhave clang-format first

--- format.sh
+++ format.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if ! command -v clang-format &> /dev/null
+then
+    echo "clang-format could not be found. Please install it first."
+    exit 1
+fi
+
 find . -name "*.cpp" -o -name "*.h" | while read file; do
     clang-format -i "$file"
     echo "Formatted $file"
