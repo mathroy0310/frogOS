4e412ad15ca413ae6e45f43cf7ea73e8eece904f
mathroy0310 <mathroy0310@gmail.com>
2024-08-28 01:18:24 -0400

    fix some errors/asserts stuff with froglib

--- FROG/include/FROG/Assert.h
+++ FROG/include/FROG/Assert.h
@@ -6,15 +6,19 @@
 /*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:00:38 by maroy             #+#    #+#             */
-/*   Updated: 2024/08/26 16:09:19 by maroy            ###   ########.fr       */
+/*   Updated: 2024/08/28 01:13:28 by maroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #if defined(__is_kernel)
-	#include <kernel/Panic.h>
-	#define ASSERT(cond) do { if (!(cond)) Kernel::panic("ASSERT("#cond") failed"); } while(false)
+#include <kernel/Panic.h>
+#define ASSERT(cond)                                            \
+	do {                                                        \
+		if (!(cond)) Kernel::panic("ASSERT(" #cond ") failed"); \
+	} while (false)
+#define ASSERT_NOT_REACHED() Kernel::panic("ASSERT_NOT_REACHED() failed")
 #else
-	#error "NOT IMPLEMENTED"
+#error "NOT IMPLEMENTED"
 #endif
