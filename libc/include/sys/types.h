16404fc29d62ce2379b416675b65c91e6d4dfeb2
mathroy0310 <mathroy0310@gmail.com>
2024-09-20 02:10:41 -0400

    new types in include/sys

--- libc/include/sys/types.h
+++ libc/include/sys/types.h
@@ -6,7 +6,7 @@
 /*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:00:27 by maroy             #+#    #+#             */
-/*   Updated: 2024/09/03 14:00:27 by maroy            ###   ########.fr       */
+/*   Updated: 2024/09/20 02:09:40 by maroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 
 __BEGIN_DECLS
 
-typedef int32_t pid_t;
+typedef int32_t  id_t;
+typedef id_t     pid_t;
+typedef id_t     uid_t;
+typedef id_t     gid_t;
+typedef uint16_t mode_t;
 
 __END_DECLS
